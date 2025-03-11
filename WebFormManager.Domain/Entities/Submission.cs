9e9356a3dc7a5ff249c9b9393ce3575de715da74
Ilya Shelkunov <shelkunov.20014@gmail.com>
2025-03-11 23:03:09 +0300

    feat(entity): the Submission.cs has been added.

---
+++ WebFormManager.Domain/Entities/Submission.cs
@@ -0,0 +1,8 @@
+﻿namespace WebFormManager.Domain.Entities;
+
+public class Submission
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public string Data { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+}
