d0d95e890faf67effbafa315ecc4eb5a913d1693
Ilya Shelkunov <shelkunov.20014@gmail.com>
2025-03-11 23:05:39 +0300

    feat(class): the ApplicationDbContext.cs has been added.

---
+++ WebFormManager.Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebFormManager.Domain.Entities;
+
+namespace WebFormManager.Infrastructure;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+    
+    public DbSet<Submission> Submissions { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Submission>()
+            .Property(s => s.Data)
+            .HasColumnType("jsonb");
+    }
+}
