c10313ddd913d52b83088f7e81694faf5b88492d
Ilya Shelkunov <shelkunov.20014@gmail.com>
2025-03-12 22:25:57 +0300

    feat(validator): the FormSubmissionValidator.cs has been added for validation the domain model.

---
+++ WebFormManager.Application/Validation/FormSubmissionValidator.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using FluentValidation;
+using WebFormManager.Domain.Entities;
+
+namespace WebFormManager.Application.Validation;
+
+public class FormSubmissionValidator : AbstractValidator<FormSubmission>
+{
+    public FormSubmissionValidator()
+    {
+        RuleFor(x => x.FormName)
+            .NotEmpty().WithMessage("FormName is required.");
+        
+        RuleFor(x => x.Data)
+            .NotEmpty().WithMessage("Form data cannot be empty.")
+            .Must(data => data.Count > 0).WithMessage("At least one field must be present.");
+    }
+}
