3b52cc29e9f152823379a61078623bd9fb70dee2
Ilya Shelkunov <shelkunov.20014@gmail.com>
2025-03-12 22:24:37 +0300

    feat(class): the PersistenceServiceRegistration.cs was added for DI realization with the best practies.

---
+++ WebFormManager.Infrastructure/PersistenceServiceRegistration.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+using WebFormManager.Application.Contracts.Persistence;
+using WebFormManager.Infrastructure.Persistence;
+
+namespace WebFormManager.Infrastructure;
+
+public static class PersistenceServiceRegistration
+{
+    public static void AddPersistenceServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        var storageType = configuration["Storage:Type"] ?? "InMemory";
+        
+        if (storageType == "File")
+        {
+            services.AddSingleton<ISubmissionStorage, FileSubmissionStorage>();
+        }
+        else
+        {
+            services.AddSingleton<ISubmissionStorage, InMemorySubmissionStorage>();
+        }
+        
+        Log.Information($"Using {storageType} submission storage");
+    }
+}
