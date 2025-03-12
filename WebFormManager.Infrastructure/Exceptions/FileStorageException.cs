2bc511d2eb5f4b11e9179242c5b2190c09ce7c76
Ilya Shelkunov <shelkunov.20014@gmail.com>
2025-03-12 22:11:05 +0300

    feat(exception): the FileStorageException.cs was added to was added to catch the error of working with the file.

---
+++ WebFormManager.Infrastructure/Exceptions/FileStorageException.cs
@@ -0,0 +1,7 @@
+﻿namespace WebFormManager.Infrastructure.Exceptions;
+
+public class FileStorageException : Exception
+{
+    public FileStorageException(string message, Exception innerException)
+        : base(message, innerException) { }
+}
