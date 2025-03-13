db5b346fc95d34cf8c99acafd418402eada37aac
Ilya Shelkunov <shelkunov.20014@gmail.com>
2025-03-13 23:49:20 +0300

    feat(dto): the DTOs for FormSubmission have been added.

---
+++ WebFormManager.Application/DTOs/FormSubmissionRequest.cs
@@ -0,0 +1,6 @@
+﻿namespace WebFormManager.Application.DTOs;
+
+public record class FormSubmissionRequest
+{
+    public Dictionary<string, object> Data { get; init; } = new();
+}
---
+++ WebFormManager.Application/DTOs/FormSubmissionResponse.cs
@@ -0,0 +1,3 @@
+﻿namespace WebFormManager.Application.DTOs;
+
+public record FormSubmissionResponse(string FormName, Dictionary<string, object> Data, DateTime SubmittedAt);
