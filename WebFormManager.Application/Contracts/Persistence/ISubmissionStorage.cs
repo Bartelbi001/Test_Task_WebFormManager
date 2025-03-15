aee3d1026784618823dfbf53f7946b1e1c2d3bb2
Ilya Shelkunov <shelkunov.20014@gmail.com>
2025-03-15 22:40:52 +0300

    chore(interface): the ISubmissionStorage.cs has been deleted from Persistence layer.

--- WebFormManager.Application/Contracts/Persistence/ISubmissionStorage.cs
+++
@@ -1,9 +0,0 @@
-﻿using WebFormManager.Domain.Entities;
-
-namespace WebFormManager.Application.Contracts.Persistence;
-
-public interface ISubmissionStorage
-{
-    Task SaveAsync(FormSubmission submission);
-    Task<List<FormSubmission>> GetAllAsync();
-}
