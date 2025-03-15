cb8822a0f88e26bb7da57728dccc9f397bf756cf
Ilya Shelkunov <shelkunov.20014@gmail.com>
2025-03-15 22:53:05 +0300

    feat(interface): the ISubmissionStorage.cs has been modified.

---
+++ WebFormManager.Application/Services/Interfaces/ISubmissionStorage.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json;
+
+namespace WebFormManager.Application.Contracts.Persistence;
+
+public interface ISubmissionStorage
+{
+    Task SaveAsync(JsonElement submission, CancellationToken cancellationToken);
+    IAsyncEnumerable<JsonElement> GetAllAsync(CancellationToken cancellationToken);
+    IAsyncEnumerable<JsonElement> SearchAsync(string query, CancellationToken cancellationToken);
+}
